254bb1088b1bd46590041abefc9efbfb646c7086
JonathanHelvey <jonathanhelvey27@gmail.com>
2019-11-03 20:47:39 -0600

    useEffect hook

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -1,9 +1,14 @@
 /* eslint-disable react/no-array-index-key */
-import React from "react"
+import React, { useState, useEffect } from "react"
 
 import { StyledCarousel } from "../carousel/Carousel.styled.js"
 
 const Carousel = () => {
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    document.title = `You clicked ${count} times`
+  })
   return (
     <StyledCarousel>
       <div>
@@ -11,6 +16,8 @@ const Carousel = () => {
           src={require("../../data/images/EricBanner1.jpg")}
           alt="EricRyanSwanson"
         />
+        <p>You clicked {count} times</p>
+        <button onClick={() => setCount(count + 1)}>Click me</button>
       </div>
     </StyledCarousel>
   )
