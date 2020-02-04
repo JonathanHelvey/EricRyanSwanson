4543840f27b9f786ef3cd78d5b3daf6ca1d0afbe
JonathanHelvey <jonathanhelvey27@gmail.com>
2020-02-03 18:06:42 -0600

    delete cache

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 import Layout from '../components/Layout/Layout';
 import SEO from '../components/SEO';
 import ArtistBio from '../components/ArtistBio';
-import Hero from '../components/Hero';
+import Hero from '../components/Hero/Hero';
 
 
 const IndexPage = () => (
