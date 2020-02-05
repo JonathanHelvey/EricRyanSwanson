0d93425aa3d48d14abc4ad7dea20a0f6b1b70d50
JonathanHelvey <jonathanhelvey27@gmail.com>
2020-02-05 11:21:28 -0600

    Resize image 164

---
+++ src/components/NewsFeed/NewsFeed.js
@@ -0,0 +1,76 @@
+
+import React from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
+import { useStaticQuery, graphql } from 'gatsby';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+
+import {
+  NewsBox,
+  Wrapper,
+  ScrollDiv,
+  NewsHeadlineStyle,
+  NewsDateStyle,
+  NewsCard,
+} from './NewsFeed.styled';
+import { Header2, theme } from '../../themes/theme';
+
+const useStyles = makeStyles({
+  card: {
+    minWidth: 50,
+    margin: 10,
+    backgroundColor: theme.primaryLight,
+  },
+  title: {
+    fontSize: 14,
+  },
+});
+
+const NewsFeed = () => {
+  const classes = useStyles();
+  const data = useStaticQuery(graphql`
+  query {
+    newsQuery: allMarkdownRemark (
+      sort: {fields: frontmatter___priority} , 
+      filter: {fileAbsolutePath: {regex: "/News/i"}}
+    ) {
+      nodes {
+        id
+        frontmatter {
+          title
+          priority
+          date
+          info
+        }
+        html
+      }
+    }
+  }
+`);
+  return (
+    <>
+      <Header2>Latest News</Header2>
+      <NewsBox>
+        <Wrapper>
+          <ScrollDiv>
+            {data.newsQuery.nodes.map(({ frontmatter, id, html }) => (
+              <NewsCard key={`key=${id}`}>
+                <Card className={classes.card}>
+                  <CardContent className={classes.title}>
+                    <NewsHeadlineStyle>{frontmatter.title}</NewsHeadlineStyle>
+                    <NewsDateStyle>{frontmatter.date}</NewsDateStyle>
+                    <div dangerouslySetInnerHTML={{ __html: html }} />
+                  </CardContent>
+                </Card>
+              </NewsCard>
+            ))}
+          </ScrollDiv>
+        </Wrapper>
+      </NewsBox>
+    </>
+  );
+};
+
+
+export default NewsFeed;
---
+++ src/components/NewsFeed/NewsFeed.styled.js
@@ -0,0 +1,54 @@
+import styled from 'styled-components';
+
+export const NewsBox = styled.div`
+  /* background-color: ${({ theme }) => theme.primaryGreen}; */
+  /* margin-top: 2vh;  */
+  /* margin-bottom: 2vh; */
+  padding: 5% 2vw; /* Added a percentage value for top/bottom padding to keep the wrapper inside of the parent */
+  
+  /* -webkit-transform: skewY(-5deg);
+  -moz-transform: skewY(-5deg);
+  -ms-transform: skewY(-5deg);
+  -o-transform: skewY(-5deg);
+  transform: skewY(-5deg); */
+`;
+
+export const Wrapper = styled.div`
+  background-color: ${({ theme }) => theme.primaryLightDark};
+  /* margin-top: 1vh; */
+  padding: 5% 20px; /* Added a percentage value for top/bottom padding to keep the wrapper inside of the parent */
+  
+  /* -webkit-transform: skewY(5deg);
+  -moz-transform: skewY(5deg);
+  -ms-transform: skewY(5deg);
+  -o-transform: skewY(5deg);
+  transform: skewY(5deg); */
+  `;
+
+export const ScrollDiv = styled.div`
+  overflow-y: scroll; 
+  overflow-x: hidden;
+  max-height: 50vh;
+`;
+
+export const NewsCard = styled.h4`
+  color: ${({ theme }) => theme.primaryLight};
+  text-align: center;
+  font-weight: bold;
+`;
+
+export const NewsHeadlineStyle = styled.h4`
+  color: ${({ theme }) => theme.primaryPurple};
+  text-align: center;
+`;
+
+export const NewsDateStyle = styled.h6`
+  color: ${({ theme }) => theme.primaryLightDark};
+  text-align: center;
+`;
+
+export const StyledSocialLogos = styled.div`
+  width: 5vw;
+  height: 5vh;
+  margin: 10px;
+`;
