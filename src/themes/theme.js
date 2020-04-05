75dea41c6829118bc34142762f8e306f017e7420
JonathanHelvey <jonathanhelvey27@gmail.com>
2020-04-05 16:32:27 -0400

    syntax fix

--- src/themes/theme.js
+++ src/themes/theme.js
@@ -26,7 +26,7 @@ export const Section1 = styled.div`
   flex-direction: column;
   align-items: center;
   padding: 8vw;
-  height: 90vh:
+  height: 90vh;
   width: auto;
   background-color: ${(props) => (props.Primary ? theme.primaryLightDark : theme.primaryLight)};
   @media (max-width: ${() => theme.mobile.md}) {
